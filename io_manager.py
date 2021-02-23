03b70cc124f8bd6fb70255e1036a7fe921bcf606
AleMarcomini <72344163+AleMarcomini@users.noreply.github.com>
2021-02-23 18:05:56 +0100

    Final release

--- io_manager.py
+++ io_manager.py
@@ -3,7 +3,7 @@ import serial
 def anticomp2(x):
   if x <= 127: return x
   else: return x-256
-ser = serial.Serial('/dev/ttyUSB17', baudrate=115200) # open serial port
+ser = serial.Serial('/dev/ttyUSB7', baudrate=115200) # open serial port
 print(ser.name) # check which port was really used
 signal = []
 output = [] 
@@ -14,9 +14,9 @@ for sig in signal:
     ser.write(chr(sig))
     d = ser.read()
     output.append(ord(d))
-output = [anticomp2(output(i)) for i in range(0,len(output))]
+output = [anticomp2(output[i]) for i in range(0,len(output))]
 file2 = open("output_vectors.txt", "w")
 for o in output:
-  file2.write(o)
+  file2.write(str(o))
   file2.write("\n")
 file2.close()
